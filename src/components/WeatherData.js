8a4a1100990c5dd1a762cffd8868ddaceecd86c8
Estevan Defaz <macbookpro@MacBook-Pro-de-Bryan.local>
2018-12-09 20:12:31 -0500

    constants

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import WheatherExtraInfo from './WeatherExtraInfo';
 import WheatherTemperature from './WheatherTemperature';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WYNDY,
+} from './../constants/weathers';
 
 const WeatherData = () => (
     <div> 
-        <WheatherTemperature temperature ={20} weatherState={"sunny"}></WheatherTemperature>
+        <WheatherTemperature temperature ={20} weatherState={RAIN}></WheatherTemperature>
         <WheatherExtraInfo humidity= {80} wind={"10 m/s"}></WheatherExtraInfo>
     </div>
 );
--- src/components/WheatherTemperature.js
+++ src/components/WheatherTemperature.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
+
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WYNDY,
+} from './../constants/weathers';
+
 const icons ={
-    sunny: "day-sunny",
-    fog: "day-fog"
+    [CLOUD]: "cloud" ,
+    [CLOUDY]: "cloudy",
+    [SUN]: "day-sunny",
+    [RAIN]: "rain",
+    [SNOW]: "snow",
+    [WYNDY]: "windy",
 };
+
 const getWeatherIcon = weatherState => {
     const icon = icons[weatherState];
     if(icon){

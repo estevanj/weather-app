0faaaaf8ad06114e0ecf34ebfaa0bd3dac5d1bf1
Estevan Defaz <macbookpro@MacBook-Pro-de-Bryan.local>
2018-12-16 17:42:12 -0500

    Style Location

--- src/components/WeatherLocation/Location.js
+++ src/components/WeatherLocation/Location.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import './style.css';
 
 const Location = (props) => {
     //Destructuring  ES6
     const { city } = props;
 
-    return (<div><h1>{city}</h1></div>);
+    return (<div className="locationCont">
+        <h1>{city}</h1>
+        </div>);
 };
 
 Location.propTypes ={

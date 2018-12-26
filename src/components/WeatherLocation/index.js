eccd3b9f125d9388252c969dfeb2f2d120e480d8
Estevan Defaz <macbookpro@MacBook-Pro-de-Bryan.local>
2018-12-25 23:43:05 -0500

    mensaje de cargando

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,32 +1,25 @@
 import React, { Component }  from 'react';
-import Location from './Location'
-import WeatherData from './WeatherData'
-import { api_weather } from './../../constants/api_url'
+import Location from './Location';
+import WeatherData from './WeatherData';
+import { api_weather } from './../../constants/api_url';
 import './style.css';
-import transformWeather from './../../services/transfotmWeather'
-import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    SNOW,
-    WINDY,
-} from './../../constants/weathers';
-
-const data ={
-    temperature: 5,
-    weatherState: SUN,
-    humidity: 20,
-    wind : '10 m/s',
-}
+import transformWeather from './../../services/transfotmWeather';
 
 class  WeatherLocation extends Component{
     constructor(){
         super();
         this.state ={
             city: 'Buenos Aires',
-            data: data,
+            data: null,
         }
+    }
+
+    componentDidMount(){
+        this.handleIpdateClick();
+    }
+
+    componentDidUpdate(prevProps, prevState){
+
     }
 
     handleIpdateClick = () => {
@@ -41,12 +34,14 @@ class  WeatherLocation extends Component{
     }
 
     render() {
+        const { city, data} = this.state
         return (
             <div className="weatherLocationCont">
-            <Location city={this.state.city}></Location>
-            <WeatherData data= {this.state.data}></WeatherData>
-            <button onClick= {this.handleIpdateClick}>Actualizar</button>
-            </div>
+            <Location city={city}></Location>
+           {data ? <WeatherData data= {data}></WeatherData> 
+           : "cargando..."
+           }   
+           </div>
         )
     }
   
